e65045a4644bc75739c78322b3a0fe736280da63
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2021-10-06 23:13:33 -0500

    Updated GeneralEventListener

--- src/main/java/io/github/misterbug5/discordbot/listeners/GeneralEventListener.java
+++ src/main/java/io/github/misterbug5/discordbot/listeners/GeneralEventListener.java
@@ -3,15 +3,15 @@ package io.github.misterbug5.discordbot.listeners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class GeneralEventListener implements EventListener {
+public class GeneralEventListener extends ListenerAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(GeneralEventListener.class);
 
     @Override
-    public void onEvent(GenericEvent arg0) {
-        LOGGER.info(arg0.toString());
+    public void onGuildJoin(GuildJoinEvent event) {
+        LOGGER.info("I Joined in " + event.getGuild().getId());
+        super.onGuildJoin(event);
     }
-    
 }
