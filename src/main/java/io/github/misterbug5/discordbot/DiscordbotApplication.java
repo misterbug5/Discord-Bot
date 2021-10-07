2aeff70527380c3259980b2c6ee24f64038f0a47
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2021-10-06 20:12:05 -0500

    A litle test, please ignore

--- src/main/java/io/github/misterbug5/discordbot/DiscordbotApplication.java
+++ src/main/java/io/github/misterbug5/discordbot/DiscordbotApplication.java
@@ -1,5 +1,10 @@
 package io.github.misterbug5.discordbot;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 import javax.security.auth.login.LoginException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +19,14 @@ import net.dv8tion.jda.api.JDABuilder;
 @SpringBootApplication
 public class DiscordbotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscordbotApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		URL whatismyip = new URL("http://checkip.amazonaws.com");
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+						whatismyip.openStream()));
+
+		String ip = in.readLine(); //you get the IP as a String
+		System.out.println(ip);
+		//SpringApplication.run(DiscordbotApplication.class, args);
 	}
 
 	@Bean
