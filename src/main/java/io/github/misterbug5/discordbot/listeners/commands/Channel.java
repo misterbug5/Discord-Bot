1f493e2d6bcd4558d34fce78a591f182cddad3eb
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2021-11-03 15:36:52 -0500

    Created channel command (needs rework)

--- src/main/java/io/github/misterbug5/discordbot/listeners/commands/Channel.java
+++ src/main/java/io/github/misterbug5/discordbot/listeners/commands/Channel.java
@@ -1,6 +1,7 @@
 package io.github.misterbug5.discordbot.listeners.commands;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -8,6 +9,10 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
 import io.github.misterbug5.discordbot.entities.Server;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 
 public class Channel implements ICommand {
@@ -24,7 +29,7 @@ public class Channel implements ICommand {
 
     @Override
     public String getHelp() {
-        return "manage your current channel\nCommands: setadminchannel, setnotifychannel, setmusicchannel, setlogchannel, settextvisibility, setvoicevisibility";
+        return "manage your current channel\nCommands: adminchannel, notifychannel, musicchannel, logchannel, textvisibility, voicevisibility";
     }
 
     @Override
@@ -44,18 +49,18 @@ public class Channel implements ICommand {
             context.getChannel().sendMessageFormat("You are in %s", context.getChannel().getName()).queue();
             return;
         }
-        if (args.get(0).equalsIgnoreCase("setadminchannel")) {
+        if (args.get(0).equalsIgnoreCase("adminchannel")) {
             setAdminChannel(context.getGuild().getId(), context.getChannel().getId());
             context.getChannel().sendMessage("Successfully set\nNow all commands here are executed as admin\nSee all changes here").queue();
             return;
         }
-        if (args.get(0).equalsIgnoreCase("setnotifychannel")) {
+        if (args.get(0).equalsIgnoreCase("notifychannel")) {
             setNotifyChannel(context.getGuild().getId(), context.getChannel().getId());
             context.getChannel().sendMessage("Successfully set\nNow all notifications are shown here\nSee all changes here").queue();
             return;
         }
-        if (args.get(0).equalsIgnoreCase("setmusicchannel")) {
-            VoiceChannel channel = context.getGuildEvent().getMember().getVoiceState().getChannel();
+        if (args.get(0).equalsIgnoreCase("musicchannel")) {
+            VoiceChannel channel = context.getVoiceChannel();
             if (channel == null) {
                 context.getChannel().sendMessage("You need to be in a voice channel").queue();
                 return;
@@ -64,30 +69,78 @@ public class Channel implements ICommand {
             context.getChannel().sendMessage("Successfully set\nNow this will be the music channel\nSee all changes here").queue();
             return;
         }
-        if (args.get(0).equalsIgnoreCase("setlogchannel")) {
+        if (args.get(0).equalsIgnoreCase("logchannel")) {
             setLogChannel(context.getGuild().getId(), context.getChannel().getId());
             context.getChannel().sendMessage("Successfully set\nNow all logs are shown here\nSee all changes here").queue();
             return;
         }
-        if (args.get(0).equalsIgnoreCase("settextvisibility")) {
-            setTextChannelVisibility(context.getGuild().getId(), context.getChannel().getId());
-            context.getChannel().sendMessage("Successfully set\nNow only roles mentioned can see this channel\nSee all changes here").queue();
+        if (args.get(0).equalsIgnoreCase("textvisibility")) {
+            boolean mentioned = false;
+            if(!context.getMessage().getMentionedRoles().isEmpty()){
+                setChannelVisibilityRole(context.getTextChannel(), context.getMessage().getMentionedRoles());
+                mentioned = true;
+            }
+            if(!context.getMessage().getMentionedMembers().isEmpty()){
+                setChannelVisibilityMember(context.getTextChannel(), context.getMessage().getMentionedMembers());
+                mentioned = true;
+            }
+            if (mentioned) {
+                context.getChannel().sendMessage("Successfully set\nNow only roles mentioned can see this channel\nSee all changes here").queue();
+            } else {
+                context.getChannel().sendMessage("You must mention a user or role").queue();
+            }
             return;
         }
-        if (args.get(0).equalsIgnoreCase("setvoicevisibility")) {
-            setVoiceChannelVisibility(context.getGuild().getId(), context.getChannel().getId());
-            context.getChannel().sendMessage("Successfully set\nNow only roles mentioned can see this channel\nSee all changes here").queue();
+        if (args.get(0).equalsIgnoreCase("voicevisibility")) {
+            VoiceChannel channel = context.getVoiceChannel();
+            if (channel == null) {
+                context.getChannel().sendMessage("You need to be in a voice channel").queue();
+                return;
+            }
+            boolean mentioned = false;
+            if(!context.getMessage().getMentionedRoles().isEmpty()){
+                setChannelVisibilityRole(channel, context.getMessage().getMentionedRoles());
+                mentioned = true;
+            }
+            if(!context.getMessage().getMentionedMembers().isEmpty()){
+                setChannelVisibilityMember(channel, context.getMessage().getMentionedMembers());
+                mentioned = true;
+            }
+            if (mentioned) {
+                context.getChannel().sendMessage("Successfully set\nNow only roles mentioned can see this channel\nSee all changes here").queue();
+            } else {
+                context.getChannel().sendMessage("You must mention a user or role").queue();
+            }
             return;
         }
     }
 
-    private void setVoiceChannelVisibility(String id, String channel) {
+    private void setChannelVisibilityMember(TextChannel channel, List<Member> mentionedMembers) {
+        for (Member member : mentionedMembers) {
+            channel.upsertPermissionOverride(member).setAllow(Permission.VIEW_CHANNEL).queue();
+        }
     }
 
-    private void setTextChannelVisibility(String id, String channel) {
+    private void setChannelVisibilityRole(TextChannel channel, List<Role> mentionedRoles) {
+        for (Role role : mentionedRoles) {
+            channel.upsertPermissionOverride(role).setAllow(Permission.VIEW_CHANNEL).queue();
+        }
+    }
+
+    private void setChannelVisibilityRole(VoiceChannel channel, List<Role> mentionedRoles) {
+        for (Role role : mentionedRoles) {
+            channel.upsertPermissionOverride(role).setAllow(Permission.VIEW_CHANNEL, Permission.VOICE_CONNECT).queue();
+        }
+    }
+
+    private void setChannelVisibilityMember(VoiceChannel channel, List<Member> mentionedMembers) {
+        for (Member member : mentionedMembers) {
+            channel.upsertPermissionOverride(member).setAllow(Permission.VIEW_CHANNEL, Permission.VOICE_CONNECT).queue();
+        }
     }
 
     private void setLogChannel(String id, String channel) {
+        database.findAndModify(Query.query(Criteria.where("_id").is(id)), new Update().set("logChannel", channel), Server.class);
     }
 
     private void setMusicChannel(String id, String channel) {
@@ -95,6 +148,7 @@ public class Channel implements ICommand {
     }
 
     private void setNotifyChannel(String id, String channel) {
+        database.findAndModify(Query.query(Criteria.where("_id").is(id)), new Update().set("notificationsChannel", channel), Server.class);
     }
 
     private void setAdminChannel(String id, String channel) {
--- src/main/java/io/github/misterbug5/discordbot/listeners/commands/CommandContext.java
+++ src/main/java/io/github/misterbug5/discordbot/listeners/commands/CommandContext.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 
@@ -22,6 +25,9 @@ public class CommandContext {
     private String prefix;
     private PrivateMessageReceivedEvent privateEvent;
     private GuildMessageReceivedEvent guildEvent;
+    private Member member;
+    private VoiceChannel voiceChannel;
+    private TextChannel textChannel;
 
     public CommandContext(ArrayList<String> argList, ArrayList<ICommand> commands, PrivateMessageReceivedEvent event, String prefix) {
         this.commands = commands;
@@ -35,6 +41,9 @@ public class CommandContext {
         this.prefix = prefix;
         this.privateEvent = event;
         this.guildEvent = null;
+        this.member = null;
+        this.voiceChannel = null;
+        this.textChannel = null;
     }
 
     public CommandContext(ArrayList<String> argList, ArrayList<ICommand> commands, GuildMessageReceivedEvent event, String prefix) {
@@ -49,6 +58,9 @@ public class CommandContext {
         this.prefix = prefix;
         this.privateEvent = null;
         this.guildEvent = event;
+        this.member = event.getMember();
+        this.voiceChannel = event.getMember().getVoiceState().getChannel();
+        this.textChannel = event.getChannel();
     }
 
     public User getUser() {
@@ -93,6 +105,18 @@ public class CommandContext {
 
     public GuildMessageReceivedEvent getGuildEvent() {
         return guildEvent;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public VoiceChannel getVoiceChannel() {
+        return voiceChannel;
+    }
+
+    public TextChannel getTextChannel() {
+        return textChannel;
     }
 
 }
